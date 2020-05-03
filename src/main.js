713cb82c91c985be54687d24232e8b3f9b6350d0
Ricardo Neves <rsn491@gmail.com>
2020-05-03 19:57:12 +0200

    #1 Add timer to trivia game (#15)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import CircularCountDownTimer from "vue-circular-count-down-timer";
+
+Vue.use(CircularCountDownTimer);
 Vue.config.productionTip = false
 
 new Vue({
