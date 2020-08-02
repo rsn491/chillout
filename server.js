639163fb13a142133c8f26444320be9c85f01176
Ricardo Neves <rsn491@gmail.com>
2020-08-02 21:31:39 +0200

    Update project structure

--- server.js
+++ server.js
@@ -1,45 +1,40 @@
 const express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const {RoomRepo} = require('./backend/infrastructure/roomRepo.js');
-const {TriviaRepo} = require('./backend/infrastructure/triviaRepo.js');
+const {RoomRepo} = require('./src/server/roomRepo.js');
+const {TriviaRepo} = require('./src/server/triviaRepo.js');
 
 const app = express();
 const port = 3000;
 const jsonParser = bodyParser.json();
 
 const roomRepo = new RoomRepo();
-const trivaRepo = new TriviaRepo();
+const triviaRepo = new TriviaRepo();
 
 app.use(cors());
 
 app.get('/api/trivia', jsonParser, async function (req, res) {
   const numberOfQuestions = req.query.numberOfQuestions;
 
-  const triviaQuestions = await trivaRepo.getTriviaQuestions(numberOfQuestions);
-
+  const triviaQuestions = await triviaRepo.getTriviaQuestions(numberOfQuestions);
   res.json(triviaQuestions);
 });
 
 app.post('/api/room', jsonParser, function (req, res) {
   const peerId = req.body.peerId;
 
-  const roomId = roomRepo.createRoom(peerId);
-
+  const roomId = roomRepo.create(peerId);
   res.json({ roomId });
 });
 
 app.post('/api/room/:roomId/join', jsonParser, function (req, res) {
-  const peerId = req.body.peerId;
   const roomId = req.params.roomId;
 
-  const room = roomRepo.addPeer(roomId, peerId);
-
-  res.json(room);
+  res.json(roomRepo.get(roomId));
 });
 
 app.use('/', express.static('dist'))
 app.use('*', express.static('dist/index.html'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`listening at http://localhost:${port}`))
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+      .entry("app")
+      .clear()
+      .add("./src/client/main.js")
+      .end();
+  },
   devServer: {
     disableHostCheck: true,
-  }
+  },
 }
